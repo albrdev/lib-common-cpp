fdf869b301f783e9d0b8af51693d6d2e253817f3
albrdev <albrdev@gmail.com>
2022-04-02 21:38:25 +0200

    Function comparison

---
+++ src/common/Common.hpp
@@ -0,0 +1,21 @@
+#ifndef __COMMON__COMMON_HPP__
+#define __COMMON__COMMON_HPP__
+
+namespace Common
+{
+  template<typename T, typename... U>
+  bool CompareFunctions(std::function<T(U...)> a, std::function<T(U...)> b)
+  {
+    if(!a || !b)
+    {
+      return (!a) && (!b);
+    }
+
+    using type    = T(U...);
+    type** rawLhs = a.template target<type*>();
+    type** rawRhs = b.template target<type*>();
+    return (*rawLhs) == (*rawRhs);
+  }
+} // namespace Common
+
+#endif // __COMMON__COMMON_HPP__
---
+++ src/common/Common.test.cpp
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+#include "Common.hpp"
+#include <functional>
+
+using namespace ::testing;
+using namespace Common;
+
+namespace UnitTest
+{
+  static void func1() {}
+  static void func2() {}
+
+  TEST(Common, Basics)
+  {
+    {
+      std::function<void()> a = nullptr;
+      std::function<void()> b = nullptr;
+      ASSERT_TRUE(CompareFunctions(a, b));
+    }
+
+    {
+      std::function<void()> a = func1;
+      std::function<void()> b = func1;
+      ASSERT_TRUE(CompareFunctions(a, b));
+    }
+
+    {
+      std::function<void()> a = func1;
+      std::function<void()> b = func2;
+      ASSERT_FALSE(CompareFunctions(a, b));
+    }
+  }
+} // namespace UnitTest
